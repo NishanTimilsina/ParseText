f470481024d9309fd5a3cec3ed52ad113a81db2c
damnhansy <timilnishan@gmail.com>
2016-04-19 21:01:39 -0500

    parse data from only 2 category description and skills

--- ParseTextProject/ViewController.swift
+++ ParseTextProject/ViewController.swift
@@ -7,22 +7,27 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var displayLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //var resumeDict:[String:NSMutableArray]=[:]
         var desArray=[String]()
+       // let desArray:[String]=["Database"," SAP","DB2","UDP\n","IOS","Objective C","REST","Core Data","GCD","-","\t","$5","performance", "measures", "based", "on", "your", "workload\n-      ", "Check", "for", "automatic", "actions", "DB2", "has", "taken", "on", "your", "behalf\n-      ", "Ensure", "there", "is", "enough", "free", "memory\n-      ", "Manual", "Verification", "using", "Check", "Points", "on", "Table", "Spaces,", "Verify", "total", "storage", "on", "UNIX", "systems", "and", "Database", "snapshots\n-      ", "Verify", "parameters", "for", "Virtual", "Memory,", "Swap"]
         var resumeArray=[String]()
         var filteredDesArray=[String]()
         var count:Int=0;
-        resumeArray=readDataFromFile("resume")!
-        desArray=readDataFromFile("description")!
+        resumeArray=readDataFromFile("resume2")!
+        
+        desArray=readDataFromDes("description")!
         for filterText in desArray{
         filteredDesArray.append(removeSpecialCharsFromString(filterText))
         }
-        print(filteredDesArray)
+        print(resumeArray)
         //print(desArray)
         
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0))
@@ -33,6 +38,8 @@ class ViewController: UIViewController {
                 }
             }
             dispatch_async(dispatch_get_main_queue()) {
+                print(count)
+                print(filteredDesArray.count)
                 self.displayLabel.text="\(count*100/filteredDesArray.count)% matched found!!!!"            }
         }
         
@@ -48,10 +55,58 @@ class ViewController: UIViewController {
         //print("\(url)")
 
         do {
-            let text = try NSString( contentsOfURL: url, encoding: NSUTF8StringEncoding)
-           return text.componentsSeparatedByString(" ")
+            let text = try String( contentsOfURL: url, encoding: NSUTF8StringEncoding)
             
+            if(text.containsString("Job Description")||(text.containsString("Skills"))){
+            // Search for one string in another.
+                var resArr=[String]()
+                    var result = text.rangeOfString("Job Description:",
+                        options: NSStringCompareOptions.LiteralSearch,
+                        range: (text as String).startIndex..<(text as String).endIndex,
+                        locale: nil)
+            
+                    var result1 = text.rangeOfString("Skills:",
+                        options: NSStringCompareOptions.LiteralSearch,
+                        range: (text as String).startIndex..<(text as String).endIndex,
+                        locale: nil)
+                    // See if string was found.
+                            if let range = result {
+                                    // Start of range of found string.
+                                    var start = range.startIndex
+                                    // Display string starting at first index.
+                               resArr=(text[start..<result1!.startIndex]).componentsSeparatedByString(" ")
 
+                }
+                if let range1 = result1 {
+                    // Start of range of found string.
+                    var start1 = range1.startIndex
+                    // Display string starting at first index.
+                    resArr+=(text[start1..<text.endIndex]).componentsSeparatedByString(" ")
+
+                }
+               return  resArr
+
+
+            }
+            return text.componentsSeparatedByString(" ")
+
+
+        } catch let errOpening as NSError {
+            print("Error!", errOpening)
+            return nil
+        }
+        
+    }
+    
+    func readDataFromDes(filename:String)->[String]?{
+        let url = NSBundle.mainBundle().URLForResource(filename, withExtension: "txt")!
+        //print("\(url)")
+        
+        do {
+            let text = try NSString( contentsOfURL: url, encoding: NSUTF8StringEncoding)
+            return text.componentsSeparatedByString(" ")
+            
+            
         } catch let errOpening as NSError {
             print("Error!", errOpening)
             return nil
